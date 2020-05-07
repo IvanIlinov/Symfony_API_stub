8a39b46308d8e8472de5fc185c9e6e3efb3bcd2e
IvanIlinov <i.am.ivan.ilinov@gmail.com>
2020-05-07 14:05:22 +0300

    Add work with DataBase from Controllers: Student, Subject

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,54 +3,98 @@
 
 namespace App\Controller;
 
-
-
+use App\Entity\Student;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use JMS\Serializer\SerializerBuilder;
 
 class StudentController extends AbstractController
 {
-    public function createStudent(){
-        return new Response('OK', 200);
+    /*
+     * checkData
+     */
+    /*
+     * request->query->get("name")
+     * request->getContent
+     */
+
+    public function createStudent(Request $_request){
+
+        $name = $_request->query->get("name");
+        if(is_string($name)){
+            $entityManager = $this->getDoctrine()->getManager();
+            $student = new Student();
+            $student->setName($name);
+            $entityManager->persist($student);
+            $entityManager->flush();
+            return new JsonResponse($student->getId());
+        }
+
+        return new Response('Invalid data', '422');
     }
 
     public function getStudent($id){
 
-        return $response = new JsonResponse(
-            [
-                'student' =>
-                    [
-                        'id' => $id,
-                        'name' => 'Jhon Watson',
-                    ]
-            ]
-        );
+        $repository = $this->getDoctrine()->getRepository(Student::class);
+        $student = $repository->find($id);
+
+        if (!$student) {
+            return new Response("Not Found", 404);
+        }
+
+        $serializer = SerializerBuilder::create()->build();
+        $jsonContent = $serializer->serialize($student, 'json');
+
+        return JsonResponse::fromJsonString($jsonContent);
     }
 
-    public function updateStudent($_id){
-        return new Response('OK', 200);
+    public function updateStudent(int $id, Request $_request){
+        $name = $_request->query->get("name");
+
+        if(is_string($name)){
+            $entityManager = $this->getDoctrine()->getManager();
+            $repository = $this->getDoctrine()->getRepository(Student::class);
+            $student = $repository->find($id);
+            $student = $student->setName($name);
+            $entityManager->persist($student);
+            $entityManager->flush();
+
+            $serializer = SerializerBuilder::create()->build();
+            $jsonContent = $serializer->serialize($student, 'json');
+
+            return JsonResponse::fromJsonString($jsonContent);
+        }
+
+        return new Response('Invalid data', 422);
     }
 
-    public function deleteStudent($_id){
+    public function deleteStudent(int $id){
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Student::class);
+        $student = $repository->find($id);
+
+        if (!$student) {
+            return new Response('Not found', 404);
+        }
+
+        $entityManager->remove($student);
+        $entityManager->flush();
+
         return new Response('OK', 200);
     }
 
     public function getStudents(){
-        return $response = new JsonResponse(
-            [
-                '0' =>
-                    [
-                        'id' => '0',
-                        'name' => 'Jhon Watson',
-                    ],
-                '1' =>
-                    [
-                        'id' => '1',
-                        'name' => 'Bilbo Begins',
-                    ]
-            ]
-        );
+
+        $repository = $this->getDoctrine()->getRepository(Student::class);
+        $students = $repository->findAll();
+
+        $serializer = SerializerBuilder::create()->build();
+        $jsonContent = $serializer->serialize($students, 'json');
+
+        return JsonResponse::fromJsonString($jsonContent);
     }
+
 }
--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -3,54 +3,90 @@
 
 namespace App\Controller;
 
-
-
+use App\Entity\Subject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use JMS\Serializer\SerializerBuilder;
 
 class SubjectController extends AbstractController
 {
-    public function createSubject(){
-        return new Response('OK', 200);
+    public function createSubject(Request $_request){
+
+        $name = $_request->query->get("name");
+        if(is_string($name)){
+            $entityManager = $this->getDoctrine()->getManager();
+            $subject = new Subject();
+            $subject->setName($name);
+            $entityManager->persist($subject);
+            $entityManager->flush();
+            return new JsonResponse($subject->getId());
+        }
+
+        return new Response('Invalid data', '422');
     }
 
     public function getSubject($id){
-        return $response = new JsonResponse(
-            [
-                'subject' =>
-                    [
-                        'id' => $id,
-                        'name' => 'Alchemy',
-                    ]
-            ]
-        );
+
+        $repository = $this->getDoctrine()->getRepository(Subject::class);
+        $subject = $repository->find($id);
+
+        if (!$subject) {
+            return new Response("Not Found", 404);
+        }
+
+        $serializer = SerializerBuilder::create()->build();
+        $jsonContent = $serializer->serialize($subject, 'json');
+
+        return JsonResponse::fromJsonString($jsonContent);
     }
 
-    public function updateSubject($_id){
-        return new Response('OK', 200);
+    public function updateSubject(int $id, Request $_request){
+        $name = $_request->query->get("name");
+
+        if(is_string($name)){
+            $entityManager = $this->getDoctrine()->getManager();
+            $repository = $this->getDoctrine()->getRepository(Subject::class);
+            $subject = $repository->find($id);
+            $subject = $subject->setName($name);
+            $entityManager->persist($subject);
+            $entityManager->flush();
+
+            $serializer = SerializerBuilder::create()->build();
+            $jsonContent = $serializer->serialize($subject, 'json');
+
+            return JsonResponse::fromJsonString($jsonContent);
+        }
+
+        return new Response('Invalid data', 422);
     }
 
-    public function deleteSubject($_id){
+    public function deleteSubject(int $id){
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Subject::class);
+        $subject = $repository->find($id);
+
+        if (!$subject) {
+            return new Response('Not found', 404);
+        }
+
+        $entityManager->remove($subject);
+        $entityManager->flush();
+
         return new Response('OK', 200);
     }
 
     public function getSubjects(){
-        return $response = new JsonResponse(
-            [
-                '0' =>
-                    [
-                        'id' => '0',
-                        'name' => 'Potions',
-                    ],
-                '1' =>
-                    [
-                        'id' => '1',
-                        'name' => 'Learn to learn',
-                    ]
-            ]
-        );
+
+        $repository = $this->getDoctrine()->getRepository(Subject::class);
+        $subjects = $repository->findAll();
+
+        $serializer = SerializerBuilder::create()->build();
+        $jsonContent = $serializer->serialize($subjects, 'json');
+
+        return JsonResponse::fromJsonString($jsonContent);
     }
 
 }
